a7cf0220e708448fa84ada706c4cfe0f1cdd9522
Gregory Hawthorne <ghawthorne@me.com>
2021-02-06 13:39:31 +0100

    add disqus

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-ap
 import AltLayout from '~/layouts/AltLayout.vue'
+import VueDisqus from 'vue-disqus'
 
 // import DefaultLayout from '~/layouts/Default.vue'
 
@@ -9,4 +10,5 @@ export default function (Vue, { router, head, isClient }) {
   head.bodyAttrs = { class: "body" }
   // Set default layout as a global component
   Vue.component('Layout', AltLayout)
+  Vue.use(VueDisqus)
 }
--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,12 +1,14 @@
 <template>
     <Layout>
         <div v-html="$page.post.content"></div>
+        <Disqus shortname="gridsome-5" :identifier="$page.post.id" />
     </Layout>
 </template>
 
 <page-query>
-query($path: String!) {
+query ($path: String!) {
   post(path: $path) {
+    id
     content
   }
 }
